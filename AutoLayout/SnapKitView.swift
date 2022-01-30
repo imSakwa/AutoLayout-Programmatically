26649fdb5dc89f344c18bcaa7ecfa710120f9aea
ChangMin <sopg04@gmail.com>
2022-01-30 23:45:04 +0900

    autolayout 연습 끝!!!

--- AutoLayout/SnapKitView.swift
+++ AutoLayout/SnapKitView.swift
@@ -41,6 +41,14 @@ class SnapKitView: UIViewController {
         $0.image = UIImage(systemName: "doc.text.fill")
     }
     
+    let thisView = UIView().then {
+        $0.backgroundColor = .brown
+    }
+    
+    let thatView = UIView().then {
+        $0.backgroundColor = .darkGray
+    }
+    
     private func setLayout() {
         self.view.addSubview(bigView)
         bigView.snp.makeConstraints {
@@ -79,6 +87,23 @@ class SnapKitView: UIViewController {
             $0.size.equalTo(24)
         }
         
+        self.view.addSubview(thisView)
+        self.view.addSubview(thatView)
+        
+        thisView.snp.makeConstraints {
+//            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(80)
+            $0.leading.equalToSuperview().inset(10)
+            $0.width.equalTo(100)
+            $0.height.equalTo(30)
+        }
+        
+        thatView.snp.makeConstraints {
+            $0.leading.greaterThanOrEqualTo(thisView.snp.trailing).offset(10)
+            $0.top.equalTo(thisView.snp.top)
+            $0.width.equalTo(100)
+            $0.height.equalTo(thisView.snp.height)
+        }
 
     }
     
